0ec83810704d9685c0c1b6907e6b01b22a49e543
KASRO1 <nikita150489@mail.ru>
2024-12-26 00:02:18 +0500

    last

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
+    <meta name="description"
+        content="Мы предлагаем широкий ассортимент металлопроката, соответствующего все требованиям ГОСТ, применяемого в строительных, промышленных и производственных целях." />
+    <meta property="og:description"
+        content="Мы предлагаем широкий ассортимент металлопроката, соответствующего все требованиям ГОСТ, применяемого в строительных, промышленных и производственных целях." />
     <!-- <link rel="stylesheet" href="/assets/fonts/stylesheet.css"> -->
     <link rel="icon" href="/favicon.png" />
     @routes
